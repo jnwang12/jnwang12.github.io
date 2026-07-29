b8fa0ee0251afebfbf5a9ed4a8a5951f721dbf87
jnwang12 <joywang354@gmail.com>
2026-07-28 19:30:28 -0700

    changedd footer

--- src/components/Margins.jsx
+++ src/components/Margins.jsx
@@ -16,10 +16,15 @@ const Header = () => {
 const Footer = () => {
     return(
         <div className="footer">
-            <h2 style = {{ color: "#FFFAE9",padding: "50px"}}>let's chat! contact me at:</h2>
-            <h2 style = {{color: "#FFFAE9", padding: "50px", textDecoration: "underline"}}>jw321@rice.edu</h2>
-
+            <div className="aligning">
+                <h2 style={{color: "#FFFAE9", padding: "50px"}}>let's chat! contact me at:</h2>
+                <h2 style = {{color: "#FFFAE9", textDecoration: "underline"}}>jw321@rice.edu</h2>
+            </div>
+            <div>
+                <h2 style={{color: "#FFFAE9"}}>made with ♡ by joy!!</h2>
+            </div>
         </div>
+        
     )
 
 }
